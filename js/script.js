53bac75d8aa0087a01efed5f94e06e80d253e4af
Marek Kuźmuk <marek.kuzmuk@gmail.com>
2019-10-15 22:09:03 +0200

    getting addition info on clicking a title

--- js/script.js
+++ js/script.js
@@ -1,14 +1,36 @@
 $(document).ready(function () {
 
-    var $books = $('#books');
+    var $titles = $('.title');
 
     $.ajax({
         url: 'http://localhost:8282/books/',
         type: 'GET',
         daraType: 'json'
     }).done(function (result) {
+        var $booksList = $('#books-list');
         $.each(result, function (i, book) {
-            $books.append('<li style="list-style: none">' + book.title + '</li><div></div>');
+            $booksList.append("<li data-no='" + book.id + "'>" + '<b>' + book.title + '</b>' + '</li><div></div>');
+
         });
+
+        $booksList.on("click", "li", function () {
+            var clickedLi = $(this);
+            $.ajax({
+                url: "http://localhost:8282/books/" + clickedLi.data("no"),
+                contentType: "application/json",
+                method: "GET"
+            }).done(function (result) {
+                clickedLi.next().html(
+                    "Author: " + result.author + "<br/>" +
+                    "Publisher: " + result.publisher + "<br/>" +
+                    "Type: " + result.type + "<br/>" +
+                    "ISBN: " + result.isbn + "<br/>" +
+                    "ID: " + result.id);
+
+            });
+
+        });
+
     });
+
 });
