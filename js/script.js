eefffcd99a96d4fb6d60b0877d7767219d7627b3
Marek Kuźmuk <marek.kuzmuk@gmail.com>
2019-10-15 21:21:09 +0200

    displaying book titles

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-   alert("it lives!") ;
+
+    var $books = $('#books');
+
+    $.ajax({
+        url: 'http://localhost:8282/books/',
+        type: 'GET',
+        daraType: 'json'
+    }).done(function (result) {
+        $.each(result, function (i, book) {
+            $books.append('<li style="list-style: none">' + book.title + '</li><div></div>');
+        });
+    });
 });
