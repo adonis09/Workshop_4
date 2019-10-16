84ae446edb3ae60cb85610f70c78c2261c87554a
Marek Kuźmuk <marek.kuzmuk@gmail.com>
2019-10-16 07:23:14 +0200

    add book form

--- js/script.js
+++ js/script.js
@@ -1,34 +1,57 @@
 $(document).ready(function () {
 
-    var $titles = $('.title');
+    var $booksList = $('#books-list');
+    function loadBooks() {
+        $.ajax({
+            url: 'http://localhost:8282/books/',
+            type: 'GET',
+            daraType: 'json'
+        }).done(function (result) {
+            $.each(result, function (i, book) {
+                $booksList.append("<li data-no='" + book.id + "'>" + '<b>' + book.title + '</b>' + '</li><div></div>');
+            });
 
-    $.ajax({
-        url: 'http://localhost:8282/books/',
-        type: 'GET',
-        daraType: 'json'
-    }).done(function (result) {
-        var $booksList = $('#books-list');
-        $.each(result, function (i, book) {
-            $booksList.append("<li data-no='" + book.id + "'>" + '<b>' + book.title + '</b>' + '</li><div></div>');
+        });
+    }
+
+    loadBooks();
+
+    $booksList.on("click", "li", function () {
+        var clickedLi = $(this);
+        $.ajax({
+            url: "http://localhost:8282/books/" + clickedLi.data("no"),
+            contentType: "application/json",
+            method: "GET"
+        }).done(function (result) {
+            clickedLi.next().html(
+                "Author: " + result.author + "<br/>" +
+                "Publisher: " + result.publisher + "<br/>" +
+                "Type: " + result.type + "<br/>" +
+                "ISBN: " + result.isbn + "<br/>" +
+                "ID: " + result.id);
 
         });
 
-        $booksList.on("click", "li", function () {
-            var clickedLi = $(this);
-            $.ajax({
-                url: "http://localhost:8282/books/" + clickedLi.data("no"),
-                contentType: "application/json",
-                method: "GET"
-            }).done(function (result) {
-                clickedLi.next().html(
-                    "Author: " + result.author + "<br/>" +
-                    "Publisher: " + result.publisher + "<br/>" +
-                    "Type: " + result.type + "<br/>" +
-                    "ISBN: " + result.isbn + "<br/>" +
-                    "ID: " + result.id);
+    });
 
-            });
+    var formSubmit = $("#submit");
+    formSubmit.on("click", function () {
 
+        var newBook = {"isbn": $("#isbn").val(),
+            "title": $("#title").val(),
+            "author": $("#author").val(),
+            "publisher": $("#publisher").val(),
+            "type": $("#type").val() };
+
+        $.ajax({
+            url: "http://localhost:8282/books/",
+            data: JSON.stringify(newBook),
+            contentType: "application/json",
+            method: "POST"
+
+        }).done(function () {
+            console.log("added new book");
+            loadBooks();
         });
 
     });
