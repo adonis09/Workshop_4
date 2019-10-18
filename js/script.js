8962be5121df0be41f3a4d2c4226db9f604a6e83
Marek Kuźmuk <marek.kuzmuk@gmail.com>
2019-10-18 21:24:29 +0200

    update book not working

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 $(document).ready(function () {
 
     var $booksList = $('#books-list');
+
     function loadBooks() {
         $.ajax({
             url: "http://localhost:8282/books/",
             type: "GET",
-            daraType: "application/json"
+            contentType: "application/json"
         }).done(function (result) {
             $booksList.empty();
             $.each(result, function (i, book) {
                 $booksList.append("<li data-no='" + book.id + "'>" + '<b>' + book.title + '</b>' +
-                    "<button id='"+ book.id +"'>delete</button></li><div></div>");
-                $('#' + book.id).on('click',function (del) {
+                    "<button id='" + book.id + "'>delete</button></li><div></div>");
+                $('#' + book.id).on('click', function (del) {
                     del.stopPropagation();
                     $.ajax({
                         url: "http://localhost:8282/books/" + book.id,
                         method: "DELETE"
-                    }).done(function() {
+                    }).done(function () {
                         loadBooks();
                     })
 
@@ -37,14 +38,41 @@ $(document).ready(function () {
             contentType: "application/json",
             method: "GET"
         }).done(function (result) {
-            if(clicksCount % 2 != 0){
+            if (clicksCount % 2 != 0) {
                 clickedLi.next().html(
-                    "Author: " + result.author + "<br/>" +
-                    "Publisher: " + result.publisher + "<br/>" +
-                    "Type: " + result.type + "<br/>" +
-                    "ISBN: " + result.isbn + "<br/>" +
-                    "ID: " + result.id);
-            }else{
+                    "<form>" +
+                    "    <input id=\"id\" type=\"hidden\" value=" + result.id +
+                    "    ISBN: <input id=\"isbn\" type=\"number\" value=" + result.isbn + "><br>" +
+                    "    Title: <input id=\"title\" type=\"text\" value=" + result.title + "><br>" +
+                    "    Author: <input id=\"author\" type=\"text\" value=" + result.author + "><br>" +
+                    "    Publisher:<input id=\"publisher\" type=\"text\" value=" + result.publisher + "><br>" +
+                    "    Type:<input id=\"type\" type=\"text\" value=" + result.type + "><br>" +
+                    "    <input type=\"submit\" value=\"Update book\" id='u" + result.id + "'>" +
+                    "</form><br>");
+
+                $('#u' + result.id).on('click', function (update) {
+                    console.log("result id: " + result.id);
+                    var updateBook = {
+                        "id": $("#id").val(),
+                        "isbn": $("#isbn").val(),
+                        "title": $("#title").val(),
+                        "author": $("#author").val(),
+                        "publisher": $("#publisher").val(),
+                        "type": $("#type").val()
+                    };
+                    console.log("book object:" + JSON.stringify(updateBook));
+                    $.ajax({
+                        url: "https://localhost:8282/books/" + result.id,
+                        data: JSON.stringify(updateBook),
+                        contentType: "application/json",
+                        method: "PUT"
+                    }).done(function () {
+                        loadBooks();
+                        console.log('PUT went well!');
+                    });
+                })
+
+            } else {
                 clickedLi.next().empty();
             }
 
@@ -55,11 +83,13 @@ $(document).ready(function () {
     var formSubmit = $("#submit");
     formSubmit.on("click", function () {
 
-        var newBook = {"isbn": $("#isbn").val(),
+        var newBook = {
+            "isbn": $("#isbn").val(),
             "title": $("#title").val(),
             "author": $("#author").val(),
             "publisher": $("#publisher").val(),
-            "type": $("#type").val() };
+            "type": $("#type").val()
+        };
 
         $.ajax({
             url: "http://localhost:8282/books/",
@@ -73,10 +103,4 @@ $(document).ready(function () {
 
     });
 
-    var delButts = $('.delButtons');
-    delButts.removeClass('delButtons');
-
-
 });
-
-
